8af13f7898da0c8fda53cbeb057cac66569a34b3
John Kennedy Loria Segundo <jk.1989@hotmail.com>
2026-07-29 23:16:26 -0300

    awsm theme colors: minor aesthetical adjustments

--- .config/awesome/themes/jkyon/color_palette.lua
+++ .config/awesome/themes/jkyon/color_palette.lua
@@ -1,11 +1,11 @@
 --[[
 --       Title:      color_palette.lua
---       Brief:      
+--       Brief:
 --       Path:       /home/jkyon/.config/awesome/themes/jkyon/color_palette.lua
 --       Author:     John Kennedy a.k.a. jKyon
 --       Created:    2026-03-21
 --       Updated:    2026-03-21
---       Notes:      
+--       Notes:
 --]]
 
 
